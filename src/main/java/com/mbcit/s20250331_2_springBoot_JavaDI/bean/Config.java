526feacef4ae5ff420524ef40acb0bcd407e4677
KimSooRim <111556128+KimSooRim@users.noreply.github.com>
2025-04-17 12:20:33 +0900

    config 클래스 주석 지움

--- src/main/java/com/mbcit/s20250331_2_springBoot_JavaDI/bean/Config.java
+++ src/main/java/com/mbcit/s20250331_2_springBoot_JavaDI/bean/Config.java
@@ -3,12 +3,9 @@ package com.mbcit.s20250331_2_springBoot_JavaDI.bean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//	springBoot 설정에 사용되는 클래스는 @Configuration 어노테이션을 붙여준다.
 @Configuration
 public class Config {
 	
-//	bean 설정에 사용되는 메서드는 @Bean 어노테이션을 붙여준다.
-//	Member 클래스의 bean을 만든다.
 	@Bean
 	public Member member1() {
 		Member member = new Member();
@@ -18,8 +15,6 @@ public class Config {
 		return member;
 	}
 	
-//	@Bean 어노테이션의 name 속성을 생성할 bean의 이름을 지정할 수 있다.
-//	name 속성을 사용하지 않으면 메서드 이름이 bean의 이름으로 등록된다.
 	@Bean(name = "hello")
 	public Member member2() {
 		
@@ -28,13 +23,11 @@ public class Config {
 		return member;
 	}
 	
-//	PrinterA 클래스의 bean을 만든다.
 	@Bean
 	public PrinterA printerA() {
 		return new PrinterA();
 	}
 	
-//	PrinterB 클래스의 bean을 만든다.
 	@Bean
 	public PrinterB printerB() {
 		return new PrinterB();
